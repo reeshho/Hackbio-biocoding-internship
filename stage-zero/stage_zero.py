51021e5d2541c66ea2bfedafe5d23367ce07f20a
Priyanjali Chowdhury <priyanjali150803@gmail.com>
2025-02-07 19:01:10 +0530

    Create stage_zero.py

---
+++ stage-zero/stage_zero.py
@@ -0,0 +1,9 @@
+name = "Priyanjali Chowdhury"
+slack_username = "Priyanjali"
+email = "priyanjali150803@gmail.com"
+hobbies = "Listening to music"
+country = "India"
+discipline = "Bioinformatics"
+programming_language = "Python"
+
+print(f"Name: {name}\nSlack Username: {slack_username}\nEmail: {email}\nHobbies: {hobbies}\nCountry: {country}\nDiscipline: {discipline}\nPreferred Programming Language: {programming_language}")
